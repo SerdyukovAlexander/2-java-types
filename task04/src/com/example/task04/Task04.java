2ab59245f90e8c842b2301176ea9e595f620d1a9
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-18 20:17:35 +0500

    Задание 4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -2,20 +2,38 @@ package com.example.task04;
 
 public class Task04 {
 
-    public static float calculate(int a, int b, String operation) {
+    public static float calculate(int a, int b, String operation)
+    {
+        float result = 0;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (operation=="+")
+        {
+            result = a + b;
+        }
 
-        return 0;
+        else if(operation=="-")
+        {
+            result = a - b;
+        }
+
+        else if(operation=="*")
+        {
+            result = a*b;
+        }
+
+        else if(operation=="/")
+        {
+            result = (float) a/b;
+        }
+
+        return result;
     }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+    public static void main(String[] args)
+    {
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
