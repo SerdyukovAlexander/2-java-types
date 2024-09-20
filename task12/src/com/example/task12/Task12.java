b465f2e02e890fca4733a56f5a9d9356e7a01cde
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-20 15:37:46 +0500

    Задание 13

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -4,23 +4,18 @@ import java.math.BigDecimal;
 
 public class Task12 {
 
-    public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
-
-        // TODO раскомментируйте и исправьте код
-
-        // Считаем проценты за год
-
-        /*
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+    public static BigDecimal benefit(BigDecimal sum, BigDecimal percent)
+    {
+        for (int i = 1; i <= 12; i++)
+        {
+            sum = sum.add(sum.multiply(percent));
         }
-        return sum;
-        */
 
-        return BigDecimal.ZERO;
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
         BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
--- task13/src/com/example/task13/Task13.java
+++ task13/src/com/example/task13/Task13.java
@@ -2,20 +2,16 @@ package com.example.task13;
 
 public class Task13 {
 
-    public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
-        return c;
+    public static char toUpperCase(char c)
+    {
+        char result = Character.toUpperCase(c);
+        return result;
     }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
+    public static void main(String[] args)
+    {
+        char result = toUpperCase('x');
         System.out.println(result);
-         */
     }
 
 }
