8d032d664a5550e7d7fe5583effb13f89e08804b
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-09-20 13:50:28 +0500

    Задание 7

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -1,21 +1,22 @@
 package com.example.task07;
 
-public class Task07 {
+public class Task07
+{
+    public static int solution(int n, int m, int k)
+    {
+        int cntTents = 0;
+        int cntWidth, cntLength = 0; // количество полных палаток которые могут поместиться в ширину и длину
 
-    public static int solution(int n, int m, int k) {
+        cntWidth = n/k;
+        cntLength = m/k;
+        cntTents = cntWidth*cntLength;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return cntTents;
     }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+    public static void main(String[] args)
+    {
         int result = solution(10, 20, 5);
         System.out.println(result);
-        */
     }
-
 }
